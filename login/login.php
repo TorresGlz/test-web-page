f83ad56df656ddaf77a97918c1dd594a4c9b3f80
TorresGlz <50779817+TorresGlz@users.noreply.github.com>
2022-09-15 16:41:56 -0500

    agregando login y crud

---
+++ login/login.php
@@ -0,0 +1,34 @@
+<?php 
+ //Conexion con la base de datos.
+ $conexion= new mysqli("localhost","root", "");
+   
+ if($conexion->connect_errno){
+      echo "Fallo al conectar a MySQL:(". $conexion->connect_errno.")";
+ }
+ else{
+ $conexion->select_db("test");
+      
+ //declaramos como variables a los campos de texto del formulario.
+ $nombre=$_POST["txtuser"];
+ $password=$_POST["txtpass"];
+
+ //Consulta del usuario y el password
+ $consulta="SELECT usuario,password FROM usuario 
+ WHERE usuario='$nombre' and password='$password'";
+ if($query= $conexion->query($consulta)){
+ $row=$query->fetch_array(); 
+ $nr =$query->num_rows; 
+ //Si existe el usuario lo va a redireccionar a la pagina de Bienvenida.
+ if($nr == 1){ 
+   header ("Location:alumno.php"); 
+ }
+ //Si no existe lo va a enviar al login otra vez.
+ else if($nr <= 0) { 
+               header("Location:index.html"); 
+ }  
+ }
+ else{
+ echo $conexion->error;  
+ }
+}  
+?>
