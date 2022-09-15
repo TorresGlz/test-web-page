a626ff1cf64731041f623d5acd114a4c7a5f60e2
TorresGlz <50779817+TorresGlz@users.noreply.github.com>
2022-09-15 18:35:12 -0500

    Add files via upload

---
+++ login/true.php
@@ -0,0 +1,20 @@
+<?php
+$con=mysqli_connect("localhost","root","1234","dbtest");
+$user=$_POST['user']
+$password=$_POST['pass']
+session_start();
+$_SESSION['user']=$user;
+$query="select * from Users where user='$user' and pass='$pass'";
+$result=mysqli_query($con, $query);
+$rows=mysqli_num_rows($result);
+
+if($rows){
+    header("location:newpage.php");
+}else{
+    ?>
+    <?php
+    include("../index.html")
+    <?php
+}
+msqli_free_result($result);
+mysqli_close($con);
