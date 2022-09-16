3f19ddb0cf93de48a1d5d1e1ab9de26ef7f8f608
TorresGlz <50779817+TorresGlz@users.noreply.github.com>
2022-09-15 19:36:15 -0500

    Add files via upload

---
+++ login/script.sql
@@ -0,0 +1,8 @@
+use dbtest;
+
+create table Users(
+    user varchar(15),
+    pass varchar(15)
+);
+
+insert into Users values('admin','admin');
---
+++ login/true.php
@@ -0,0 +1,22 @@
+<?php
+$con=mysqli_connect("localhost","root","1234","dbtest");
+$user=$_POST['user'];
+$pass=$_POST['pass'];
+session_start();
+$_SESSION['user']=$user;
+$query="SELECT*FROM Users where user='$user' and pass='$pass'";
+$result=mysqli_query($con, $query);
+$rows=mysqli_num_rows($result);
+
+if($rows){
+    header("location:newpage2.php");
+}else{
+    ?>
+    <?php
+    include("index.php")
+    ?>
+    <h1>Error</h1>
+    <?php
+}
+mysqli_free_result($result);
+mysqli_close($con);
