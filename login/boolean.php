eb5953688fbac1d450f605ecbd9561c1e4d97e44
TorresGlz <50779817+TorresGlz@users.noreply.github.com>
2022-09-15 19:36:04 -0500

    Delete login directory

--- login/boolean.php
+++
@@ -1,3 +0,0 @@
-<?php
-$con=mysqli_connect("172.26.0.4","root","1234","dbtest");
-?>
--- login/script.sql
+++
@@ -1,8 +0,0 @@
-use dbtest;
-
-create table Users(
-    user varchar(15),
-    pass varchar(15)
-);
-
-insert into Users values('admin','admin');
